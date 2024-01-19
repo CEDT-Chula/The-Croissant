900f0e14a2304af24fe3045d1483547e4d5d04c5
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2024-01-19 15:20:34 +0700

    Update package.json with reload script and Ping command works now

---
+++ src/index.js
@@ -0,0 +1,65 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_fs_1 = __importDefault(require("node:fs"));
+const node_path_1 = __importDefault(require("node:path"));
+const discord_js_1 = require("discord.js");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({
+    path: node_path_1.default.join(__dirname, "..", ".env"),
+});
+const client = new discord_js_1.Client({
+    intents: [discord_js_1.GatewayIntentBits.Guilds],
+});
+client.commands = new discord_js_1.Collection();
+const foldersPath = node_path_1.default.join(__dirname, "commands");
+const commandFolders = node_fs_1.default.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+    const commandsPath = node_path_1.default.join(foldersPath, folder);
+    const commandFiles = node_fs_1.default
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const filePath = node_path_1.default.join(commandsPath, file);
+        const command = require(filePath);
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        }
+        else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+client.once(discord_js_1.Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+client.on(discord_js_1.Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand())
+        return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+    try {
+        await command.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        }
+        else {
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        }
+    }
+});
+client.login(process.env.DISCORD_TOKEN);
